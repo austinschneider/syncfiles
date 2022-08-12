99b3c5f65ba2c831c54606101be22bce63a03a5d
Austin Schneider <hogenshpogen@gmail.com>
2022-08-12 10:21:47 -0600

    Shallow clone of repos. Use non-release pathogen.

--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@ mkdir -p ${HOME}/.vim
 mkdir -p ${HOME}/.vim/colors && cd ${HOME}/.vim/colors && wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
 xrdb -merge ${HOME}/.Xresources
 mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle
-#curl -LSso ${HOME}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ${HOME}/.vim
-git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/tpope/vim-pathogen.git --depth=1
 cd ${HOME}/.vim/autoload
 ln -s ../vim-pathogen/autoload/pathogen.vim ./pathogen.vim
 cd ${HOME}/.vim/vim-pathogen
-git checkout tags/v2.3
 cd ${HOME}/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone git://github.com/tpope/vim-commentary.git
-git clone git://github.com/tpope/vim-surround.git
-git clone https://github.com/ervandew/supertab
-git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/scrooloose/nerdtree.git --depth=1
+git clone https://github.com/scrooloose/syntastic.git --depth=1
+git clone https://github.com/tpope/vim-commentary.git --depth=1
+git clone https://github.com/tpope/vim-surround.git --depth=1
+git clone https://github.com/ervandew/supertab.git --depth=1
+git clone https://github.com/majutsushi/tagbar.git --depth=1
 cp ${HOME}/syncfiles/patches/tagbar.patch ${HOME}/.vim/bundle/tagbar/plugin/
 patch ${HOME}/.vim/bundle/tagbar/plugin/tagbar.vim -bnN -i ${HOME}/.vim/bundle/tagbar/plugin/tagbar.patch
 mkdir -p ${HOME}/.vim/plugin
